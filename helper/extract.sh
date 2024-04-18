fc0742e9ebaf67c6a50f4c38d59459596e0a6c5d
daem0nc0re <33651482+daem0nc0re@users.noreply.github.com>
2024-04-19 08:52:15 +0900

    Update extract.sh

--- helper/extract.sh
+++ helper/extract.sh
@@ -6,7 +6,7 @@ if [[ $# -ne 1 ]] ; then
 fi
 
 for s in $(objdump -d $1 | grep -E '[0-9a-f]+:' | cut -f 1 | cut -d : -f 2) ; do
-    echo -n $s | awk '{for (i = 7; i > 0; i -= 2) {printf "\\x" substr($0, i, 2)} }'
+    echo -n $s | awk '{for (i = 7; i > 0; i -= 2) {printf "\\x" substr($0, i, 2)}}'
 done
 
 echo
